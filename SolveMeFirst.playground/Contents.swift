6d8f3a9009b8b433fb7f7938760eab594169b3d4
woody-iOS <59425581+woody-iOS@users.noreply.github.com>
2021-12-13 17:37:09 +0900

    create project

---
+++ SolveMeFirst.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+/*
+Example
+a = 7
+b = 3
+ 
+Return 10
+
+Function Description
+
+Complete the solveMeFirst function in the editor below.
+
+solveMeFirst has the following parameters:
+
+int a: the first value
+int b: the second value
+ 
+Returns
+- int: the sum of  and
+
+Constraints
+1 <= a, b <= 1000
+
+Sample Input
+a = 2
+b = 3
+ 
+Sample Output
+5
+ 
+Explanation
+2 + 3 = 5
+.
+*/
+
+
+var a = Int(readLine()!)!
+var b = Int(readLine()!)!
+
+print(a + b)
