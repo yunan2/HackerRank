66f63cd700147a4317d97fe939e15c3c7515eb66
yunan2 <shapa222@naver.com>
2022-01-26 15:57:18 +0900

    Add Sales By Match

---
+++ SalesByMatch.playground/Contents.swift
@@ -0,0 +1,56 @@
+import Foundation
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+func sockMerchant(n: Int, ar: [Int]) -> Int {
+    // Write your code here
+        
+    var dict: [Int: Int] = [:]
+    var sum = 0
+    
+    for i in ar {
+        if dict[i] == nil {
+            dict[i] = 1
+        } else {
+            dict[i] = dict[i]! + 1
+        }
+    }
+    
+    for (_, v) in dict {
+        if v >= 2 {
+            sum += (v / 2)
+        }
+    }
+
+    return sum
+}
+
+
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+guard let arTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+let ar: [Int] = arTemp.split(separator: " ").map {
+    if let arItem = Int($0) {
+        return arItem
+    } else { fatalError("Bad input") }
+}
+
+guard ar.count == n else { fatalError("Bad input") }
+
+let result = sockMerchant(n: n, ar: ar)
+
+fileHandle.write(String(result).data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
